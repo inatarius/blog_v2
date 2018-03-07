fc3c6df0625280e70d6e4d40af297b8a87c69153
Ilya Natarius <ilyanatarius@Ilyas-MacBook-Pro.local>
2018-03-06 22:25:39 -0600

    Start of backend, added Flask shell for console use

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'blog_v2'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+
+    def __init__(self, username):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
 
 @app.route("/")
 def home():
